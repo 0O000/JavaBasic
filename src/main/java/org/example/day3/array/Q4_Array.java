b958b4a20f707383adc841b5a4db88f54b83a508
mino <zkzk_001@naver.com>
2024-09-30 14:17:04 +0900

    commit: 영화예약

---
+++ src/main/java/org/example/day3/array/Q4_Array.java
@@ -0,0 +1,30 @@
+package org.example.day3.array;
+
+import java.util.Scanner;
+
+public class Q4_Array {
+    public static void main(String[] args) {
+        int[] movie = new int[10];
+        int sum = 0;
+        Scanner sc = new Scanner(System.in);
+
+        while(true){
+            System.out.println("현재 좌석 상태: ");
+            for (int i = 0; i < movie.length; i++) {
+                System.out.print((i + 1) + ":" + movie[i] + " ");
+            }
+            System.out.println();
+            System.out.print("예매할 좌석 번호를 입력하세요 (종료하려면 0입력): ");
+            int seat = sc.nextInt() - 1;
+            if(seat == -1){
+                break;
+            }
+            movie[seat] += 1;
+            System.out.println(seat + 1 + "번 좌석이 예매 되었습니다. ");
+            sum += 1000;
+        }
+        System.out.println("총 예매금액: "+sum +"원");
+    }
+
+
+}
