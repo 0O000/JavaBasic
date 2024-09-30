b104b39f31eff4c39192dc5a01ed0a39d268a2e6
mino <zkzk_001@naver.com>
2024-09-30 09:38:19 +0900

    commit: 점수분석

--- src/main/java/org/example/day3/array/Q3_Array.java
+++ src/main/java/org/example/day3/array/Q3_Array.java
@@ -7,16 +7,49 @@ public class Q3_Array {
 
         Scanner sc = new Scanner(System.in);
 
+        int studentNum = 0;
+        int[] score = new int[studentNum];
+
         while(true){
             int select = 0;
             System.out.println("-----------------------------------------------");
             System.out.println("1.학생수 | 2.점수입력 | 3.점수리스트 | 4.분석 | 5.종료");
             System.out.println("-----------------------------------------------");
-            System.out.println("선택>");
+            System.out.print("선택>");
             select = sc.nextInt();
             if(select == 1){
-                System.out.println("학생수>");
-                int[] student = new int[sc.nextInt()];
+                System.out.print("학생수>");
+                studentNum = sc.nextInt();
+                score = new int[studentNum];
+            }
+            else if(select == 2){
+                if(!(studentNum == 0)){
+                    for(int i = 0; i < score.length; i++){
+                        System.out.print("score[" + i + "]>");
+                        score[i] = sc.nextInt();
+                    }
+                }
+            } else if (select == 3) {
+                if(!(studentNum == 0)){
+                    for (int i = 0; i < score.length; i++) {
+                        System.out.println("score[" + i + "]: " + score[i]);
+                    }
+                }
+            }else if (select == 4) {
+                if(!(studentNum == 0)){
+                    int max = 0;
+                    int avg = 0;
+                    for (int i = 0; i < score.length; i++) {
+                        if(score[i] > max){
+                            max = score[i];
+                        }
+                        avg += score[i];
+                    }
+                    System.out.println("최고 점수: " + max);
+                    System.out.println("평균 점수: " + avg / studentNum);
+                } else if (select == 5) {
+                    break;
+                }
             }
         }
     }
