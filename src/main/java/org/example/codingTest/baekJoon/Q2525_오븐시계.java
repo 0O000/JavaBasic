4d8cb9cd8a6cba773bc69cc9d5f9c9b9804d1509
User\mino <zkzk_001@naver.com>
2024-10-10 11:09:49 +0900

    백준 Q2525

---
+++ src/main/java/org/example/codingTest/baekJoon/Q2525_오븐시계.java
@@ -0,0 +1,35 @@
+package org.example.codingTest.baekJoon;
+
+import java.util.Scanner;
+
+public class Q2525_오븐시계 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int hour = sc.nextInt();
+        int minute = sc.nextInt();
+        int add = sc.nextInt();
+
+        int calHour = calculate(hour, minute, add);
+        int ansHour = (calHour) / 60;
+        if(ansHour >= 24){
+            ansHour = ansHour - 24;
+        }
+        int ansMinute = calHour % 60;
+
+        System.out.println(ansHour + " " + ansMinute);
+
+    }
+    public static int calculate(int hour, int minute, int add) {
+        minute += (hour * 60) + add;
+        return minute;
+    }
+    public static int calculateHour(int calHour) {
+        if(calHour > 1440){
+            calHour -= 1440;
+            return calculateHour(calHour);
+        }else{
+            return calHour;
+        }
+    }
+}
