67cae50a08ea4cb19ac05ad98828ae27d83e9505
User\mino <zkzk_001@naver.com>
2024-10-23 14:14:14 +0900

    코드정리

--- src/main/java/org/example/day17/채팅/메신저A.java
+++ src/main/java/org/example/day17/채팅/메신저A.java
@@ -10,7 +10,8 @@ import java.net.InetAddress;
 
 public class 메신저A extends JFrame {
     JTextArea list;
-    메신저A(){
+
+    메신저A() {
         //여기에 코드 다하면, static이 아니여서 더 좋은 코드를 만들 수 있음.
         System.out.println("생성자 시작.");
 
@@ -43,7 +44,7 @@ public class 메신저A extends JFrame {
                 //1. input에 입력한 데이터를 가지고 와서
                 String data = input.getText();
                 //1. list 끝에 붙이세요.
-                list.append("나: "  + data + "\n");
+                list.append("나: " + data + "\n");
                 input.setText("");//2. 입력한거 지우기!
                 //3. 입력한 내용을 상대방에게 보내자. --> 예외처리!필요!
                 //전화기 역할 소켓필요.
