df29b8a35bd95c87fbf349f7519580bbc3397a21
User\mino <zkzk_001@naver.com>
2024-10-29 08:47:41 +0900

    refactor: day20 프로그래머스 옷가게할인

--- src/main/java/org/example/codingTest/programmers/옷가게_할인_받기.java
+++ src/main/java/org/example/codingTest/programmers/옷가게_할인_받기.java
@@ -3,14 +3,17 @@ package org.example.codingTest.programmers;
 public class 옷가게_할인_받기 {
     public int solution(int price) {
         int answer = 0;
-        if(price > 100000){
-            answer = (price * 95) / 100;
+        if(price >= 100000){
+            answer = (int) (price * 0.95);
         }
-        if(price > 300000){
-            answer = (price * 90) / 100;
+        if(price >= 300000){
+            answer = (int) (price * 0.90);
         }
-        if(price > 500000){
-            answer = (price * 80) / 100;
+        if(price >= 500000){
+            answer = (int) (price * 0.80);
+        }
+        if(price < 100000){
+            answer = price;
         }
         return answer;
     }
