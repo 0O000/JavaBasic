adec3b5ac48be5582ecc3a3dfc27c81cc8cff65b
User\mino <zkzk_001@naver.com>
2024-10-31 17:34:42 +0900

    commit: day23 오후 수업

---
+++ src/main/java/org/example/day23/스레드기본/Thread1.java
@@ -0,0 +1,16 @@
+package org.example.day23.스레드기본;
+
+public class Thread1 extends Thread {
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 50; i++) {
+            System.out.println("*");
+            try {
+                sleep(500);
+            } catch (Exception e) {
+                System.out.println("스레드 재우기를 시도하다 문제 발생함");
+            }
+        }
+    }
+}
---
+++ src/main/java/org/example/day23/스레드기본/스레드메인인터페이스.java
@@ -0,0 +1,21 @@
+package org.example.day23.스레드기본;
+
+public class 스레드메인인터페이스 {
+    public static void main(String[] args) {
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("나는 별");
+            }
+        });
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("나는 달러");
+            }
+        });
+
+        t1.start();
+        t2.start();
+    }
+}
