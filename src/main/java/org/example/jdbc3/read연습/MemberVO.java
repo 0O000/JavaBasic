d3568bf1ea27ba548adffc0129dc39b7d747b82b
User\mino <zkzk_001@naver.com>
2024-11-18 08:58:50 +0900

    commit:: day33 memberVo

--- src/main/java/org/example/jdbc3/read연습/MemberVO.java
+++ src/main/java/org/example/jdbc3/read연습/MemberVO.java
@@ -6,10 +6,6 @@ public class MemberVO {
     private String name;
     private String tel;
 
-    public void setId(String id) {
-        this.id = id;
-    }
-
     public String getId() {
         return id;
     }
@@ -18,20 +14,24 @@ public class MemberVO {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getPw() {
         return pw;
     }
 
-    public void setPw(String pw) {
-        this.pw = pw;
-    }
-
     public String getTel() {
         return tel;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPw(String pw) {
+        this.pw = pw;
     }
 
     public void setTel(String tel) {
