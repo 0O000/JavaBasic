4fa06d5d04e72add080e71e16921098ef2ff7b97
User\mino <zkzk_001@naver.com>
2024-11-20 12:49:17 +0900

    commit: day36 오전 수업

---
+++ src/main/java/org/example/mongo/Member여러개삭제2.java
@@ -0,0 +1,25 @@
+package org.example.mongo;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+public class Member여러개삭제2 {
+    public static void main(String[] args) {
+        //1. MongoClient 생성
+        MongoClient client = new MongoClient("localhost", 27017);
+        //2. db연결, collection연결
+        MongoDatabase db = client.getDatabase("shop");
+        MongoCollection<Document> member = db.getCollection("member");
+        System.out.println("member연결 성공!");
+        //3. 전송할 js생성
+        //delete할 조건(filter)을 생성
+        Document filter = new Document();
+        filter.append("pw", "1234");
+        //4. 전송, 결과처리
+        member.deleteMany(filter);
+        System.out.println("몽고db에 전송함.");
+        client.close();
+    }
+}
